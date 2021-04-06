a0d771d126d695c07875ea199b3236618b4bcdc1
pyralspite413 <njv19@pitt.edu>
2021-04-05 20:31:28 -0400

    Update vmsim.java

--- vmsim.java
+++ vmsim.java
@@ -24,7 +24,7 @@ import java.io.*;
 		String memsplit; //memory split
 		String tracefile; //string containing trace file name.
 		
-		
+		String alg=args[1].toUpperCase();
 		if(args[1].equals("opt"))mode=1;
 		
 		numframes=Integer.parseInt(args[3]);
@@ -56,14 +56,12 @@ import java.io.*;
 	
 		if(mode==1) 
 			stats=opt(pagesize,numframes, proc0Frames,proc1Frames,offset,tracefile);
-			
-		System.out.println("Algorithm: "+args[1]);
+		System.out.println("Algorithm: "+alg);
 		System.out.println("Number of frames: "+numframes);
-		System.out.println("Page size: "+(pagesize/1024)+" KB");
+		System.out.println("Page size: "+pagesize+" KB");
 		System.out.println("total memory accesses: "+stats[0]);
 		System.out.println("total page faults: "+ stats[1]);
 		System.out.println("total writes to disk: "+stats[2]);
-		
 		
 		
 		}
