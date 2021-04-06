0e7b0b43a3772a5bb79a3512aa473bf1e0854188
pyralspite413 <njv19@pitt.edu>
2021-04-05 21:50:32 -0400

    Update vmsim.java

--- vmsim.java
+++ vmsim.java
@@ -58,10 +58,10 @@ import java.io.*;
 			stats=opt(pagesize,numframes, proc0Frames,proc1Frames,offset,tracefile);
 		System.out.println("Algorithm: "+alg);
 		System.out.println("Number of frames: "+numframes);
-		System.out.println("Page size: "+pagesize+" KB");
-		System.out.println("total memory accesses: "+stats[0]);
-		System.out.println("total page faults: "+ stats[1]);
-		System.out.println("total writes to disk: "+stats[2]);
+		System.out.println("Page size: "+(pagesize/1024)+" KB");
+		System.out.println("Total memory accesses: "+stats[0]);
+		System.out.println("Total page faults: "+ stats[1]);
+		System.out.println("Total writes to disk: "+stats[2]);
 		
 		
 		}
